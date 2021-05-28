72391b198c06ba830af718921f6ce3539de181f0
kavya.hk <kavya.hk@cloudlex.com>
2021-05-28 11:57:19 +0530

    bitcoin data with high chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
+import { of } from 'rxjs';
+import { concatMap, delay } from 'rxjs/operators';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'real-time-app';
+export class AppComponent implements OnInit {
+  title = 'Angular-RxJsWebSocket-HighCharts';
+  rate: any;
+  Highcharts: typeof Highcharts = Highcharts;
+  chardata: any[] = [];
+  chartOptions: any;
+  subject = webSocket('wss://ws.coincap.io/prices?assets=bitcoin');
+  constructor() {}
+
+  ngOnInit() {
+    this.rate = this.subject
+      .pipe(concatMap((item) => of(item).pipe(delay(1000))))
+      .subscribe((data) => {
+        this.rate = data;
+        this.chardata.push(Number(this.rate.bitcoin));
+        this.chartOptions = {
+          series: [
+            {
+              data: this.chardata,
+            },
+          ],
+          chart: {
+            type: 'line',
+            zoomType: 'x',
+          },
+          title: {
+            text: 'linechart',
+          },
+        };
+        console.log(data);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HighchartsChartModule } from 'highcharts-angular';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, HighchartsChartModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
